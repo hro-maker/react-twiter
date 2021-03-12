8a8673b337afbef10fb94b05f696b153e5075639
hro-maker <69071631+hro-maker@users.noreply.github.com>
2021-03-12 13:44:40 +0400

    urql

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import {Form, Formik} from 'formik'
-import { FormControl, FormErrorMessage, FormLabel } from '@chakra-ui/form-control';
-import { Input } from '@chakra-ui/input';
 import Wraper from '../components/Wrapers';
 import InputField from '../components/InpuField'
 import { Box } from '@chakra-ui/layout';
 import { Button } from '@chakra-ui/button';
+import { useMutation } from 'urql';
+import { useRegisterMutation } from '../generated/graphql';
+import { toErrormap } from '../utils/toErrormap';
+import { useRouter } from "next/router";
 interface registerProps{
 
 }
 
 const Register: React.FunctionComponent<registerProps> = (props) => {
+ const [,register]= useRegisterMutation()
+ const router=useRouter()
   return (
     <Wraper variant="small">
    <Formik
    initialValues={{username:"",password:""}}
-   onSubmit={(values)=>{
-      console.log(values)
+   onSubmit={async(values,{setErrors})=>{
+     const response=await register(values)
+     if(response.data?.register.errors){
+            setErrors(toErrormap(response.data.register.errors))
+     }else if(response.data?.register.user){
+            router.push('/')
+     }
    }}
    >
      {({isSubmitting})=>(
